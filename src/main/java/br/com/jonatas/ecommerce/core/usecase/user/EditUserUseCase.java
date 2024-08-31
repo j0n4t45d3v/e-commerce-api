d9e52552d8b5646ccfb1de8717167f0ea10da319
Jonatas <delimajonatas875@gmail.com>
2024-08-31 19:38:51 -0300

    feat: encode password edited

--- src/main/java/br/com/jonatas/ecommerce/core/usecase/user/EditUserUseCase.java
+++ src/main/java/br/com/jonatas/ecommerce/core/usecase/user/EditUserUseCase.java
@@ -1,6 +1,7 @@
 package br.com.jonatas.ecommerce.core.usecase.user;
 
 import br.com.jonatas.ecommerce.core.domain.user.UserDomain;
+import br.com.jonatas.ecommerce.gateway.in.crypto.Crypto;
 import br.com.jonatas.ecommerce.gateway.in.user.EditUserGateway;
 import br.com.jonatas.ecommerce.gateway.in.user.dto.EditUserDTO;
 import br.com.jonatas.ecommerce.gateway.out.user.UserRepositoryGateway;
@@ -9,9 +10,11 @@ import br.com.jonatas.ecommerce.infra.common.exception.NotFoundException;
 public class EditUserUseCase implements EditUserGateway {
 
 	private final UserRepositoryGateway userRepositoryGateway;
+	private final Crypto crypto;
 
-	public EditUserUseCase(UserRepositoryGateway userRepositoryGateway) {
+	public EditUserUseCase(UserRepositoryGateway userRepositoryGateway, Crypto crypto) {
 		this.userRepositoryGateway = userRepositoryGateway;
+		this.crypto = crypto;
 	}
 
 	@Override
@@ -24,7 +27,8 @@ public class EditUserUseCase implements EditUserGateway {
 
 	private void setValues(EditUserDTO userEdited, UserDomain userFound) {
 		userFound.setUsername(userEdited.username());
-		userFound.setPassword(userEdited.password());
+		String passwordEncoded = this.crypto.encode(userEdited.password());
+		userFound.setPassword(passwordEncoded);
 		userFound.setEmail(userEdited.email());
 	}
 }
