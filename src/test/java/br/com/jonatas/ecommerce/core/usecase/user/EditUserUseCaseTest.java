23daa77e5c096c8e546c32301281d8fd2a46096d
Jonatas <delimajonatas875@gmail.com>
2024-08-31 19:38:32 -0300

    test: encode password edited

--- src/test/java/br/com/jonatas/ecommerce/core/usecase/user/EditUserUseCaseTest.java
+++ src/test/java/br/com/jonatas/ecommerce/core/usecase/user/EditUserUseCaseTest.java
@@ -1,6 +1,7 @@
 package br.com.jonatas.ecommerce.core.usecase.user;
 
 import br.com.jonatas.ecommerce.core.domain.user.UserDomain;
+import br.com.jonatas.ecommerce.gateway.in.crypto.Crypto;
 import br.com.jonatas.ecommerce.gateway.in.user.dto.EditUserDTO;
 import br.com.jonatas.ecommerce.gateway.out.user.UserRepositoryGateway;
 import br.com.jonatas.ecommerce.infra.common.exception.NotFoundException;
@@ -16,8 +17,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -25,6 +25,9 @@ class EditUserUseCaseTest {
 
 	@Mock
 	private UserRepositoryGateway userRepositoryGateway;
+
+	@Mock
+	private Crypto crypto;
 
 	@InjectMocks
 	private EditUserUseCase editUserUseCase;
@@ -48,6 +51,7 @@ class EditUserUseCaseTest {
 		when(this.userRepositoryGateway.findById(anyLong())).thenReturn(Optional.of(new UserDomain()));
 		this.editUserUseCase.execute(1L, userEdited);
 		verify(this.userRepositoryGateway, Mockito.times(1)).findById(anyLong());
+		verify(this.crypto, Mockito.times(1)).encode(anyString());
 		verify(this.userRepositoryGateway, Mockito.times(1)).save(any(UserDomain.class));
 	}
 
@@ -60,6 +64,7 @@ class EditUserUseCaseTest {
 		assertEquals("User not found", thrown.getMessage());
 
 		verify(this.userRepositoryGateway, Mockito.times(1)).findById(anyLong());
+		verify(this.crypto, Mockito.times(0)).encode(anyString());
 		verify(this.userRepositoryGateway, Mockito.times(0)).save(any(UserDomain.class));
 	}
 }
