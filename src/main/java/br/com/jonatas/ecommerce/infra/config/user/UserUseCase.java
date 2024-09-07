78b7d7985c06383745320a2a609860fda9b58a7e
Jonatas <delimajonatas875@gmail.com>
2024-09-07 10:38:01 -0300

    chore: create bean methods for the user use cases

--- src/main/java/br/com/jonatas/ecommerce/infra/config/user/UserUseCase.java
+++ src/main/java/br/com/jonatas/ecommerce/infra/config/user/UserUseCase.java
@@ -1,7 +1,13 @@
 package br.com.jonatas.ecommerce.infra.config.user;
 
+import br.com.jonatas.ecommerce.core.usecase.user.DeleteUserUseCase;
+import br.com.jonatas.ecommerce.core.usecase.user.EditUserUseCase;
 import br.com.jonatas.ecommerce.core.usecase.user.RegisterUserUseCase;
+import br.com.jonatas.ecommerce.core.usecase.user.SearchUserUseCase;
+import br.com.jonatas.ecommerce.gateway.in.user.DeleteUserGateway;
+import br.com.jonatas.ecommerce.gateway.in.user.EditUserGateway;
 import br.com.jonatas.ecommerce.gateway.in.user.RegisterUserGateway;
+import br.com.jonatas.ecommerce.gateway.in.user.SearchUserGateway;
 import br.com.jonatas.ecommerce.gateway.out.user.UserRepositoryGateway;
 import br.com.jonatas.ecommerce.gateway.in.crypto.Crypto;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +15,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserUseCase {
+
 	@Bean
-	public RegisterUserGateway userUseCase(UserRepositoryGateway userRepositoryGateway, Crypto crypto) {
+	public RegisterUserGateway registerUserUseCase(UserRepositoryGateway userRepositoryGateway, Crypto crypto) {
 		return new RegisterUserUseCase(userRepositoryGateway, crypto);
 	}
+
+	@Bean
+	public SearchUserGateway searchUserUseCase(UserRepositoryGateway userRepositoryGateway) {
+		return new SearchUserUseCase(userRepositoryGateway);
+	}
+
+	@Bean
+	public EditUserGateway editUserUseCase(UserRepositoryGateway userRepositoryGateway, Crypto crypto) {
+		return new EditUserUseCase(userRepositoryGateway, crypto);
+	}
+
+	@Bean
+	public DeleteUserGateway deleteUserUseCase(UserRepositoryGateway userRepositoryGateway) {
+		return new DeleteUserUseCase(userRepositoryGateway);
+	}
+
 }
