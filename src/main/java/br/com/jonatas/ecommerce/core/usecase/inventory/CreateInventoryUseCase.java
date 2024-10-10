20fca3afc5aaa7a3f457809c26e1282b9354b1b6
Jonatas <delimajonatas875@gmail.com>
2024-10-10 19:32:24 -0300

    feat: acrescenta a logica de verificação se o produto inventariado existe

--- src/main/java/br/com/jonatas/ecommerce/core/usecase/inventory/CreateInventoryUseCase.java
+++ src/main/java/br/com/jonatas/ecommerce/core/usecase/inventory/CreateInventoryUseCase.java
@@ -3,15 +3,21 @@ package br.com.jonatas.ecommerce.core.usecase.inventory;
 import br.com.jonatas.ecommerce.core.domain.inventory.InventoryDomain;
 import br.com.jonatas.ecommerce.core.domain.inventory.ProductDomain;
 import br.com.jonatas.ecommerce.gateway.in.inventory.CreateInventoryGateway;
+import br.com.jonatas.ecommerce.gateway.in.inventory.SearchProductGateway;
 import br.com.jonatas.ecommerce.gateway.in.inventory.dto.CreateInventoryDTO;
 import br.com.jonatas.ecommerce.gateway.out.inventory.InventoryRepositoryGateway;
 
 public class CreateInventoryUseCase implements CreateInventoryGateway {
 
 	private final InventoryRepositoryGateway inventoryRepositoryGateway;
+	private final SearchProductGateway searchProductGateway;
 
-	public CreateInventoryUseCase(InventoryRepositoryGateway inventoryRepositoryGateway) {
+	public CreateInventoryUseCase(
+			InventoryRepositoryGateway inventoryRepositoryGateway,
+			SearchProductGateway searchProductGateway
+	) {
 		this.inventoryRepositoryGateway = inventoryRepositoryGateway;
+		this.searchProductGateway = searchProductGateway;
 	}
 
 	@Override
@@ -22,8 +28,7 @@ public class CreateInventoryUseCase implements CreateInventoryGateway {
 
 	private InventoryDomain createInventoryDomain(CreateInventoryDTO createInventoryDTO) {
 		InventoryDomain inventoryDomain = new InventoryDomain();
-		ProductDomain productDomain = new ProductDomain();
-		productDomain.setId(createInventoryDTO.productId());
+		ProductDomain productDomain = this.searchProductGateway.searchProductById(createInventoryDTO.productId());
 		inventoryDomain.setProduct(productDomain);
 		inventoryDomain.setQuantity(createInventoryDTO.quantity());
 		inventoryDomain.setMinimalStock(createInventoryDTO.minimalStock());
