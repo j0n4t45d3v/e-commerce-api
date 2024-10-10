2fa89a793abe54e8035a59b13b352b4793f0979f
Jonatas <delimajonatas875@gmail.com>
2024-10-10 19:54:41 -0300

    feat: cria movimentação do stock ao criar um novo inventario

--- src/main/java/br/com/jonatas/ecommerce/core/usecase/inventory/CreateInventoryUseCase.java
+++ src/main/java/br/com/jonatas/ecommerce/core/usecase/inventory/CreateInventoryUseCase.java
@@ -2,28 +2,40 @@ package br.com.jonatas.ecommerce.core.usecase.inventory;
 
 import br.com.jonatas.ecommerce.core.domain.inventory.InventoryDomain;
 import br.com.jonatas.ecommerce.core.domain.inventory.ProductDomain;
+import br.com.jonatas.ecommerce.core.domain.inventory.enums.StockMovementStatusType;
+import br.com.jonatas.ecommerce.core.domain.inventory.enums.StockMovementType;
 import br.com.jonatas.ecommerce.gateway.in.inventory.CreateInventoryGateway;
+import br.com.jonatas.ecommerce.gateway.in.inventory.CreateStockMovementGateway;
 import br.com.jonatas.ecommerce.gateway.in.inventory.SearchProductGateway;
 import br.com.jonatas.ecommerce.gateway.in.inventory.dto.CreateInventoryDTO;
+import br.com.jonatas.ecommerce.gateway.in.inventory.dto.CreateStockMovementDTO;
 import br.com.jonatas.ecommerce.gateway.out.inventory.InventoryRepositoryGateway;
+
+import java.time.LocalDateTime;
 
 public class CreateInventoryUseCase implements CreateInventoryGateway {
 
 	private final InventoryRepositoryGateway inventoryRepositoryGateway;
 	private final SearchProductGateway searchProductGateway;
+	private final CreateStockMovementGateway createStockMovementGateway;
 
 	public CreateInventoryUseCase(
 			InventoryRepositoryGateway inventoryRepositoryGateway,
-			SearchProductGateway searchProductGateway
+			SearchProductGateway searchProductGateway,
+			CreateStockMovementGateway createStockMovementGateway
 	) {
 		this.inventoryRepositoryGateway = inventoryRepositoryGateway;
 		this.searchProductGateway = searchProductGateway;
+		this.createStockMovementGateway = createStockMovementGateway;
 	}
 
 	@Override
 	public Long execute(CreateInventoryDTO createInventoryDTO) {
 		InventoryDomain inventoryDomain = this.createInventoryDomain(createInventoryDTO);
-		return inventoryRepositoryGateway.save(inventoryDomain);
+		Long inventoryId = this.inventoryRepositoryGateway.save(inventoryDomain);
+		CreateStockMovementDTO stockMovementDTO = this.createStockMovementDTO(inventoryDomain);
+		this.createStockMovementGateway.execute(stockMovementDTO, inventoryId);
+		return inventoryId;
 	}
 
 	private InventoryDomain createInventoryDomain(CreateInventoryDTO createInventoryDTO) {
@@ -34,5 +46,15 @@ public class CreateInventoryUseCase implements CreateInventoryGateway {
 		inventoryDomain.setMinimalStock(createInventoryDTO.minimalStock());
 		inventoryDomain.setReservedQuantity(0L);
 		return inventoryDomain;
+	}
+
+	private CreateStockMovementDTO createStockMovementDTO(InventoryDomain inventoryDomain) {
+		return new CreateStockMovementDTO(
+				inventoryDomain.getQuantity(),
+				StockMovementType.INPUT,
+				StockMovementStatusType.CREATED,
+				LocalDateTime.now(),
+				"Initial stock"
+		);
 	}
 }
