4abee7df5cf04000b06ecc8b9664e587f69b56db
Jonatas <delimajonatas875@gmail.com>
2024-12-25 10:59:34 -0300

    style: organize imports and reformat code

--- src/main/java/br/com/jonatas/ecommerce/gateway/out/order/OrderItemRepositoryGateway.java
+++ src/main/java/br/com/jonatas/ecommerce/gateway/out/order/OrderItemRepositoryGateway.java
@@ -1,13 +1,11 @@
 package br.com.jonatas.ecommerce.gateway.out.order;
 
-import java.util.Optional;
-
-import br.com.jonatas.ecommerce.core.domain.inventory.ProductDomain;
 import br.com.jonatas.ecommerce.core.domain.order.OrderItemDomain;
 import br.com.jonatas.ecommerce.gateway.out.CrudRepositoryGateway;
 import java.util.List;
+import java.util.Optional;
 
-public interface OrderItemRepositoryGateway extends CrudRepositoryGateway<OrderItemDomain, Long>{
-	Optional<OrderItemDomain> findByProductId(Long productId);
+public interface OrderItemRepositoryGateway extends CrudRepositoryGateway<OrderItemDomain, Long> {
+  Optional<OrderItemDomain> findByProductId(Long productId);
   List<OrderItemDomain> findAllByOrder(long anyLong);
 }
