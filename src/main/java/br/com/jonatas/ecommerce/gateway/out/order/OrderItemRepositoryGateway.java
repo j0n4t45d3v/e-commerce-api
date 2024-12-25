11db848d608bf06c386486a2379ab58a26d75edf
Jonatas <delimajonatas875@gmail.com>
2024-12-25 10:59:11 -0300

    chore: find all order items

--- src/main/java/br/com/jonatas/ecommerce/gateway/out/order/OrderItemRepositoryGateway.java
+++ src/main/java/br/com/jonatas/ecommerce/gateway/out/order/OrderItemRepositoryGateway.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import br.com.jonatas.ecommerce.core.domain.inventory.ProductDomain;
 import br.com.jonatas.ecommerce.core.domain.order.OrderItemDomain;
 import br.com.jonatas.ecommerce.gateway.out.CrudRepositoryGateway;
+import java.util.List;
 
 public interface OrderItemRepositoryGateway extends CrudRepositoryGateway<OrderItemDomain, Long>{
 	Optional<OrderItemDomain> findByProductId(Long productId);
+  List<OrderItemDomain> findAllByOrder(long anyLong);
 }
